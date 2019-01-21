51d2c88e1747adceab57f1510a642b09324f5591
kolldavi <kolldavi@gmail.com>
2019-01-21 16:57:43 -0600

    update toggle

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import './toggle.css';
-const toggleOff = {
-	width: '33%',
-	height: '100%',
-	borderRadius: '5px',
-	marginLeft: '67%',
-	backgroundColor: 'red'
+
+const styles = {
+	toggleOff: {
+		width: '33%',
+		height: '100%',
+		borderRadius: '5px',
+		marginLeft: '67%',
+		backgroundColor: 'red'
+	},
+	toggleOn: { width: '33%', height: '100%', borderRadius: '5px', backgroundColor: 'green' },
+	toggle: {
+		border: '1px solid black',
+		borderRadius: '5px',
+		width: '75px',
+		height: '30px'
+	}
 };
-const toggleOn = { width: '33%', height: '100%', borderRadius: '5px', backgroundColor: 'green' };
+
 const Toggle = ({ toggleSwitch, data, enabled }) => {
 	return (
-		<div className="toggle" onClick={toggleSwitch}>
-			<div style={enabled ? toggleOn : toggleOff}>{data}</div>
+		<div style={styles.toggle} onClick={toggleSwitch}>
+			<div style={enabled ? styles.toggleOn : styles.toggleOff}>{data}</div>
 		</div>
 	);
 };
